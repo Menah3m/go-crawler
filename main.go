62c7f4a4a55d3c08cfc3efbd8baca71bb82bc22e
menah3m <chabaokesi@outlook.com>
2023-02-09 11:45:02 +0800

    simple demo:print links count

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 /*
@@ -33,6 +34,14 @@ func main() {
 		fmt.Println("读取返回内容失败")
 	}
 
-	fmt.Println("返回结果：")
-	fmt.Println("%s", string(body))
+	// fmt.Println("返回结果：")
+	// fmt.Println("%s", string(body))
+
+	numLinks := strings.Count(string(body), "<a")
+	fmt.Printf("this html has %d links!\n", numLinks)
+
+	exist := strings.Contains(string(body), "抖音")
+
+	fmt.Printf("抖音 exist ？%v", exist)
+
 }
